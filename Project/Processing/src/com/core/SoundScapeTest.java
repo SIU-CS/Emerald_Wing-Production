0396ea421a2b1432fe70826acfa505329ac44fa4
xerxesman <31903174+xerxesman@users.noreply.github.com>
2017-12-07 22:51:20 -0600

    removed auto failing junit

--- Project/Processing/src/com/core/SoundScapeTest.java
+++ Project/Processing/src/com/core/SoundScapeTest.java
@@ -9,10 +9,7 @@ import org.junit.Test;
 public class SoundScapeTest {
 
 	String[] testString = {"Yell 'Dead Cell' VR(Mix)", "Metal Gear Solid 2", "Game Music", "Konami"};
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
 	
 	public void testMetaString() {
 		assertTrue(Arrays.equals(testString, testString));
