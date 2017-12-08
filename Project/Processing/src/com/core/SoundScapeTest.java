c2608eb643e81b7eeb16034620edc5d2f290a1d3
xerxesman <31903174+xerxesman@users.noreply.github.com>
2017-12-07 22:42:11 -0600

    figured out what test to use
    finally figured what method to use to test arrays.

--- Project/Processing/src/com/core/SoundScapeTest.java
+++ Project/Processing/src/com/core/SoundScapeTest.java
@@ -2,6 +2,8 @@ package com.core;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class SoundScapeTest {
@@ -9,6 +11,10 @@ public class SoundScapeTest {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	public void testMetaString() {
+		assertTrue(Arrays.equals(arg0, arg1));
 	}
 
 }
